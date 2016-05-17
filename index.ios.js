bc8235f80d95180f4520e36f9f1aa211d167c1ad
Weijun Yin <gw9812@gmail.com>
2016-05-17 12:09:36 +0800

    styling view with container

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,24 @@
-import React, { Component, View, Text, StyleSheet, AppRegistry } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, AppRegistry } from 'react-native';
 
 
 class Weekdays extends Component {
   render(){
-    return <View>
+    return <View style={style.container}>
       <Text>
         Days of Week
       </Text>
     </View>
   }
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 AppRegistry.registerComponent('weekdays', () => Weekdays);
 
